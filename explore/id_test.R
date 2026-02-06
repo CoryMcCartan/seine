4c2fc34b6768142653b598f2460d216a24031a44
Cory McCartan <cory.mccartan@gmail.com>
2026-02-06 01:23:39 -0500

    checkpoint ident testing

--- explore/id_test.R
+++ explore/id_test.R
@@ -5,43 +5,78 @@ library(tidyverse)
 n = 1000
 n_perm = 100
 
-run_test <- function(n = 1000, n_x = 3, p = 4, n_perm = 1000) {
+run_test <- function(n = 1000, n_x = 3, p = 2, n_perm = 1000) {
     spec0 = ei_synthetic(n, p, n_x = n_x, b_cov = 0.0004 * (1 + diag(n_x)))
     spec = ei_spec(spec0, starts_with("x"), starts_with("y"), total = attr(spec0, "ei_n"))
     # attr(spec, "b") |> hist(breaks=50)
 
-    m = ei_ridge(spec)
-
-    XZ = bases::b_tpsob(spec[, c(attr(spec, "ei_x"), attr(spec, "ei_z"))], p = 200)
+    y = as.matrix(spec0[, attr(spec0, "ei_y")])
+    X = as.matrix(spec0[, attr(spec0, "ei_x")])
+    Z = as.matrix(spec0[, attr(spec0, "ei_z")])
+    XZ0 = row_kronecker(X, Z, 1e5)
+    XZ = bases::b_tpsob(spec0[, c(attr(spec, "ei_x"), attr(spec, "ei_z"))], p = 200)
     # ZZ = bases::b_tpsob(spec[, c(attr(spec, "ei_z"))], p = 200)
 
-    udv = svd(XZ)
+    udv0 = svd(XZ0)
+    udv = svd(cbind(XZ0, XZ))
 
-    fit0 = ridge_auto(udv, resid(m), rep(1, n), vcov = FALSE)
-    r2 = diag(as.matrix(cor(fit0$fitted, resid(m))^2))
+    # fit0 = ridge_auto(udv0, y, rep(1, n), vcov = FALSE)
+    # fit = ridge_svd(udv, y, rep(1, n), vcov = FALSE, penalty = pen)
+    fit = ridge_auto(udv, y, rep(1, n), vcov = FALSE)
+    pen = fit$penalty
+    fit0 = ridge_svd(udv0, y, rep(1, n), vcov = FALSE, penalty = pen)
+    c(cor(fit0$fitted, y)^2)
+    c(cor(fit$fitted, y)^2)
+
+    rss_full = colSums((y - fit$fitted)^2)
+    rss_red = colSums((y - fit0$fitted)^2)
+    F_stat = ((rss_red - rss_full) / (fit$df - fit0$df)) / (rss_full / (n - fit$df))
 
     perm = replicate(
         n_perm,
         {
-            yy = resid(m)[sample(n), , drop = FALSE]
-            fit = ridge_svd(udv, yy, rep(1, n), penalty = fit0$penalty, vcov = FALSE)
-            diag(as.matrix(cor(fit$fitted, yy)^2))
+            yp = y[sample(n), , drop = FALSE]
+            fit = ridge_svd(udv, yp, rep(1, n), vcov = FALSE, penalty = pen)
+            fit0 = ridge_svd(udv0, yp, rep(1, n), vcov = FALSE, penalty = pen)
+            rss_full = colSums((yp - fit$fitted)^2)
+            rss_red = colSums((yp - fit0$fitted)^2)
+            F_stat = ((rss_red - rss_full) / (fit$df - fit0$df)) / (rss_full / (n - fit$df))
         },
         simplify = FALSE
     ) |>
         do.call(cbind, args = _)
 
     tibble::tibble_row(
-        r2 = r2,
-        p = (rowSums(perm >= r2) + 1) / (n_perm + 1)
+        F = F_stat,
+        p = (rowSums(perm >= F_stat) + 1) / (n_perm + 1),
+        p_param = pf(F_stat, fit$df - fit0$df, n - fit$df, lower.tail = FALSE)
     )
+
+    # r2 = diag(as.matrix(cor(fit$fitted, resid(m))^2))
+    # perm = replicate(
+    #     n_perm,
+    #     {
+    #         yy = resid(m)[sample(n), , drop = FALSE]
+    #         fit = ridge_svd(udv, yy, rep(1, n), penalty = fit0$penalty, vcov = FALSE)
+    #         diag(as.matrix(cor(fit$fitted, yy)^2))
+    #     },
+    #     simplify = FALSE
+    # ) |>
+    #     do.call(cbind, args = _)
+
+    # tibble::tibble_row(
+    #     r2 = r2,
+    #     p = (rowSums(perm >= r2) + 1) / (n_perm + 1)
+    # )
 }
 
-res = map(1:200, ~ run_test(n = 2000, n_perm = 50), .progress = TRUE) |>
+res = map(1:400, ~ run_test(n = 2000, n_perm = 50), .progress = TRUE) |>
     bind_rows()
 
 hist(res$p)
 mean(res$p <= 0.05)
+hist(res$F, breaks=50)
+hist(res$p_param, breaks=50)
 
 
 # try on wallace
